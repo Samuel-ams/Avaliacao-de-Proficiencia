de9f090ba00198c105e8d30a12deb334cd9005da
Samuel.AMS <samuelmoreira07@gmail.com>
2024-01-15 22:29:33 -0300

    Change node routing

--- node/src/app.js
+++ node/src/app.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 
 const app = express();
 
-const index = require("./routes/index");
 const tb01 = require("./routes/tb01.routes");
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +10,6 @@ app.use(express.json());
 app.use(express.json({ type: "application/vnd.api+json" }));
 app.use(cors());
 
-app.use(index);
 app.use(tb01);
 
 module.exports = app
